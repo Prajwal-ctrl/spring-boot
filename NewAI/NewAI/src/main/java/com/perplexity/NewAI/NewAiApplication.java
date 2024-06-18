b4fe4e94540d81a2d0e7b4b67e4078d7dccd6e8e
Prajwal Sontakke <63673431+Prajwal-ctrl@users.noreply.github.com>
2024-06-18 13:00:34 +0530

    Add files via upload

---
+++ NewAI/NewAI/src/main/java/com/perplexity/NewAI/NewAiApplication.java
@@ -0,0 +1,13 @@
+package com.perplexity.NewAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAiApplication.class, args);
+	}
+
+}
